9e186487996c6b8cb8ce5675117028293ee4fd63
ginto11 <salinitosnelson@gmail.com>
2023-01-12 17:00:37 -0500

    Subida del proyecto tienda

--- js/script.js
+++ js/script.js
@@ -9,8 +9,61 @@ btn.addEventListener("click", ()=>{
 
 
 document.addEventListener("click", (e) => {
+
     if(e.target.localName == "a"){
         navegacion.classList.toggle("navegacion-active");
         overlay.classList.toggle("overlay-active");
     }
+
 })
+
+
+const categorias = {
+    hombres: [
+        {nombre: "Pantalon", color: "Negro", costo: 34000}
+    ],
+    mujeres: [
+        {nombre: "Blusa", color: "Azul", costo: 45000} 
+    ],
+    niños: [
+        {nombre: "Gorro", color: "Rojo", costo: 14000}
+    ]
+};
+
+const {hombres, mujeres, niños} = categorias;
+
+document.getElementById("filtro-categorias").addEventListener("change", (e)=>{
+
+    if(e.target.value === "mujeres"){
+        mostrarDatos(mujeres);
+    }
+
+    if(e.target.value == "hombres"){
+        mostrarDatos(hombres);
+    }
+
+    if(e.target.value == "niños"){
+        mostrarDatos(niños);
+    }
+})
+
+console.log(categorias)
+
+
+function mostrarDatos(data){
+    let lista = "";
+    data.forEach(element => {
+        lista += 
+        `<ul>
+            <li> ${element.nombre} </li>
+            <li> ${element.color} </li>
+            <li> ${element.costo} </li>
+         </ul>
+        `
+    });
+
+    document.querySelector(".contenedor-productos-filtro").innerHTML = lista;
+}
+
+
+
